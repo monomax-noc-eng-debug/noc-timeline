1b0861cb1d8f3d15e39812eabe19cac56d611c69
rit-nawapat <veezalove2@gmail.com>
2025-12-30 22:13:28 +0700

    Updates

--- assets/addDays-CsCA8S13.js
+++
@@ -1,1 +0,0 @@
-import{t as s,c as i}from"./protectedTokens-BgWIJsKD.js";function c(a,r,e){const t=s(a,e?.in);return isNaN(r)?i(e?.in||a,NaN):(r&&t.setDate(t.getDate()+r),t)}export{c as a};
--- assets/addHours-vmlBnVHO.js
+++
@@ -1,1 +0,0 @@
-import{c as t,t as r,a as i}from"./protectedTokens-BgWIJsKD.js";function a(o,n,s){return t(o,+r(o)+n)}function u(o,n,s){return a(o,n*i)}export{u as a};
